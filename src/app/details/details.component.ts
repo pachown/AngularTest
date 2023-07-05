d243110542792f4981a1feeb4c52d7e4aa866bad
Thomas R McNutt <pachown@gmail.com>
2023-07-05 06:04:15 -0600

    changed misspelling of details

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-detailt',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      details works!
+    </p>
+  `,
+  styleUrls: ['./details.component.css']
+})
+export class DetailtComponent {
+
+}
