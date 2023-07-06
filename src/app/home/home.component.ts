b73003e1b9cc3ef88e8584a80c286243ef8444a9
Thomas R McNutt <pachown@gmail.com>
2023-07-06 02:52:56 -0600

    Added city search
    Implemented city search and search on active search on each key press

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,25 +13,37 @@ import { HousingService } from '../housing.service';
   ],
   template: `
   <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+    <form >
+      <input type="text" placeholder="Filter by city" (keyup)="filterResults(filter.value)" #filter >
+      <button class="primary" type="button" (click)="filterResults(filter.value)" (keyup)="filterResults(filter.value)">Search</button>
     </form>
   </section>
   <section class="results">
-    <app-housing-location
-      *ngFor="let housingLocation of housingLocationList"
-      [housingLocation]="housingLocation">
+    <app-housing-location 
+      *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation">
     </app-housing-location>
   </section>
 `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  filteredLocationList: HousingLocation[] = [];
+
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 }
